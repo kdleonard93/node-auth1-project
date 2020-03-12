4ac4e9c6133a8deeddaa0363e36c20b5ac8f5ba8
Kyle Leonard <kdleonard@icloud.com>
2020-03-11 20:13:55 -0500

    Trying to work through the the api calls

---
+++ users/user-router.js
@@ -0,0 +1,21 @@
+const express = require("express");
+const Users = require("./users-model");
+const restrict = require("../middleware/restrict");
+
+const router = express.Router();
+
+router.post("/", async (req, res, next) => {
+  try {
+    res.json(await Users.add({ id }));
+  }
+});
+
+router.get("/", restrict(), async (req, res, next) => {
+  try {
+    res.json(await Users.find());
+  } catch (err) {
+    next(err);
+  }
+});
+
+module.exports = router;
