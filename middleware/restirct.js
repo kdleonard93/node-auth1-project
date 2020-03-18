95d78a3efb5d7b0d6fb4294d749257f71354d502
Kyle Leonard <kdleonard@icloud.com>
2020-03-17 19:16:26 -0500

    Adjusted middleware

--- middleware/restirct.js
+++ middleware/restirct.js
@@ -2,7 +2,6 @@ const bcrypt = require("bcryptjs");
 const Users = require("../users/users-model");
 
 function restrict() {
-  // put in variable so we can re-use it
   const authError = {
     message: "Invalid credentials"
   };
@@ -10,24 +9,23 @@ function restrict() {
   return async (req, res, next) => {
     try {
       const { username, password } = req.headers;
-      // make sure the values aren't empty
       if (!username || !password) {
         return res.status(401).json(authError);
       }
 
       const user = await Users.findBy({ username }).first();
-      // make sure the user exists
+      // user exists
       if (!user) {
         return res.status(401).json(authError);
       }
 
       const passwordValid = await bcrypt.compare(password, user.password);
-      // make sure the password is correct
+
       if (!passwordValid) {
         return res.status(401).json(authError);
       }
 
-      // if we reach this point, the user is authenticated!
+      // user is authenticated!
       next();
     } catch (err) {
       next(err);
